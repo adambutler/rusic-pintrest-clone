74b7821e36507c480689998400d506741d1ac83a
Adam Butler <adam@littlebigmedia.co.uk>
2012-03-01 09:20:24 +0000

    styling changes

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -16,39 +16,7 @@
 
   // Styles for the iframe.
   var iframeStyle = 'position: fixed; z-index: 999999; width: 760px; height: 590px; right: 20px; top: 20px; border: 5px solid #b33732; background-color: white; scrolling: "no"; -moz-box-shadow: 0px 0px 20px #000000; -webkit-box-shadow: 0px 0px 20px #000000; box-shadow: 0px 0px 20px #000000;';
-  var closebtnStyle = "
-	  cursor:pointer; 
-	  position: fixed;
-	  right: 773px;
-	  top: 8px;
-	  z-index: 999999;
-	  border-radius:40px;
-	  width:26px;
-	  height:26px;
-	  background-color: #f5f5f5;
-	  background-image: -moz-linear-gradient(top, #ffffff, #e6e6e6);
-	  background-image: -ms-linear-gradient(top, #ffffff, #e6e6e6);
-	  background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#ffffff), to(#e6e6e6));
-	  background-image: -webkit-linear-gradient(top, #ffffff, #e6e6e6);
-	  background-image: -o-linear-gradient(top, #ffffff, #e6e6e6);
-	  background-image: linear-gradient(top, #ffffff, #e6e6e6);background-repeat: repeat-x;
-	  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff', endColorstr='#e6e6e6', GradientType=0);
-	  border-color: #e6e6e6 #e6e6e6 #bfbfbf;
-	  border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);
-	  filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);
-	  border: 1px solid #ccc;
-	  border-bottom-color: #bbb;
-	  -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-	  -moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-	  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-	  filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);
-	  line-height:26px;
-	  font-size:12px;
-	  text-align:center;
-	  font-family: Arial, sans-serif;
-	  font-weight: bold;
-	  color: #ccc;
-	  text-shadow: 1px 1px 0px #FFF;"
+  var closebtnStyle = "cursor:pointer; position: fixed;right: 773px;top: 8px;z-index: 999999;border-radius:40px;width:26px;height:26px;background-color: #f5f5f5;background-image: -moz-linear-gradient(top, #ffffff, #e6e6e6);background-image: -ms-linear-gradient(top, #ffffff, #e6e6e6);background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#ffffff), to(#e6e6e6));background-image: -webkit-linear-gradient(top, #ffffff, #e6e6e6);background-image: -o-linear-gradient(top, #ffffff, #e6e6e6);background-image: linear-gradient(top, #ffffff, #e6e6e6);background-repeat: repeat-x;filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff', endColorstr='#e6e6e6', GradientType=0);border-color: #e6e6e6 #e6e6e6 #bfbfbf;border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);border: 1px solid #ccc;border-bottom-color: #bbb;-webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);-moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);line-height:26px;font-size:12px;text-align:center;font-family: Arial, sans-serif;font-weight: bold;color: #ccc;text-shadow: 1px 1px 0px #FFF;"
 
   var images = [].slice.call(document.querySelectorAll('img'));
 
