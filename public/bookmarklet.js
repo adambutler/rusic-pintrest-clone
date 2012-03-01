63b8ce0fe7330641a48dc09ca0eef1ee08705785
Adam Butler <adam@littlebigmedia.co.uk>
2012-03-01 12:47:34 +0000

    Linked code to close btn, should alert foo

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -11,42 +11,44 @@
  
 (function() {
 
-  // This is the page that will display inside the iframe.
-  var iframeSrc = 'http://360k.rusic.com/pintrest-clone/ideas/new';
+	// This is the page that will display inside the iframe.
+	var iframeSrc = 'http://360k.rusic.com/pintrest-clone/ideas/new';
 
-  // Styles for the iframe.
-  var iframeStyle = 'position: fixed; z-index: 999999; width: 760px; height: 590px; right: 20px; top: 20px; border: 5px solid #b33732; background-color: white; scrolling: "no"; -moz-box-shadow: 0px 0px 20px #000000; -webkit-box-shadow: 0px 0px 20px #000000; box-shadow: 0px 0px 20px #000000;';
-  var closebtnStyle = "cursor:pointer; position: fixed;right: 773px;top: 8px;z-index: 999999;border-radius:40px;width:26px;height:26px;background-color: #f5f5f5;background-image: -moz-linear-gradient(top, #ffffff, #e6e6e6);background-image: -ms-linear-gradient(top, #ffffff, #e6e6e6);background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#ffffff), to(#e6e6e6));background-image: -webkit-linear-gradient(top, #ffffff, #e6e6e6);background-image: -o-linear-gradient(top, #ffffff, #e6e6e6);background-image: linear-gradient(top, #ffffff, #e6e6e6);background-repeat: repeat-x;filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff', endColorstr='#e6e6e6', GradientType=0);border-color: #e6e6e6 #e6e6e6 #bfbfbf;border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);border: 1px solid #ccc;border-bottom-color: #bbb;-webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);-moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);line-height:26px;font-size:12px;text-align:center;font-family: Arial, sans-serif;font-weight: bold;color: #ccc;text-shadow: 1px 1px 0px #FFF;"
+	// Styles for the iframe.
+	var iframeStyle = 'position: fixed; z-index: 999999; width: 760px; height: 590px; right: 20px; top: 20px; border: 5px solid #b33732; background-color: white; scrolling: "no"; -moz-box-shadow: 0px 0px 20px #000000; -webkit-box-shadow: 0px 0px 20px #000000; box-shadow: 0px 0px 20px #000000;';
+	var closebtnStyle = "cursor:pointer; position: fixed;right: 773px;top: 8px;z-index: 999999;border-radius:40px;width:26px;height:26px;background-color: #f5f5f5;background-image: -moz-linear-gradient(top, #ffffff, #e6e6e6);background-image: -ms-linear-gradient(top, #ffffff, #e6e6e6);background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#ffffff), to(#e6e6e6));background-image: -webkit-linear-gradient(top, #ffffff, #e6e6e6);background-image: -o-linear-gradient(top, #ffffff, #e6e6e6);background-image: linear-gradient(top, #ffffff, #e6e6e6);background-repeat: repeat-x;filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff', endColorstr='#e6e6e6', GradientType=0);border-color: #e6e6e6 #e6e6e6 #bfbfbf;border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);border: 1px solid #ccc;border-bottom-color: #bbb;-webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);-moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);filter: progid:DXImageTransform.Microsoft.gradient(enabled = false);line-height:26px;font-size:12px;text-align:center;font-family: Arial, sans-serif;font-weight: bold;color: #ccc;text-shadow: 1px 1px 0px #FFF;"
 
-  var images = [].slice.call(document.querySelectorAll('img'));
+	var images = [].slice.call(document.querySelectorAll('img'));
 
-  images = images.sort(function(imageA, imageB) {
-    if ((imageA.height * imageA.width) < (imageB.height * imageB.width)) {
-      return 1;
-    } else if ((imageA.height * imageA.width) > (imageB.height * imageB.width)) {
-      return -1;
-    } else {
-      return 0;
-    }
-  });
+	images = images.sort(function(imageA, imageB) {
+		if ((imageA.height * imageA.width) < (imageB.height * imageB.width)) {
+			return 1;
+		} else if ((imageA.height * imageA.width) > (imageB.height * imageB.width)) {
+			return -1;
+		} else {
+			return 0;
+		}
+	});
 
-  var pageImages = images.map(function(img) { return encodeURIComponent(img.src); });
+	var pageImages = images.map(function(img) { return encodeURIComponent(img.src); });
 
-  // Create an iframe element and set some attributes.
-  var iframe = document.createElement('iframe');
-  var closebtn = document.createElement('div');
+	// Create an iframe element and set some attributes.
+	var iframe = document.createElement('iframe');
+	var closebtn = document.createElement('div');
 
-  // Pass the current url across to the space.
-  iframe.setAttribute('src', iframeSrc + '?images[]=' + pageImages.slice(0, 9).join('&images[]='));
-  
-  iframe.setAttribute('id', 'rusic-modal');
-  iframe.setAttribute('style', iframeStyle);
-  closebtn.innerHTML = 'x';
-  closebtn.setAttribute('style', closebtnStyle);
-
-  // Inject the iframe into the host page.
-  var body = document.getElementsByTagName('body')[0];
-  body.appendChild(iframe);
-  body.appendChild(closebtn);
+	// Pass the current url across to the space.
+	iframe.setAttribute('src', iframeSrc + '?images[]=' + pageImages.slice(0, 9).join('&images[]='));
+	
+	iframe.setAttribute('id', 'rusic-modal');
+	iframe.setAttribute('style', iframeStyle);
+	
+	closebtn.innerHTML = 'x';
+	closebtn.setAttribute('style', closebtnStyle);
+	closebtn.setAttribute('onclick', "alert('foo')" style="width:100px; height:100px; background-color:red;");
+	
+	// Inject the iframe into the host page.
+	var body = document.getElementsByTagName('body')[0];
+	body.appendChild(iframe);
+	body.appendChild(closebtn);
 
 }).call(this);
